1a56230634b48a75f0d9be1c53df011b05633166
Dusty211 <kylebenz@hotmail.com>
2019-05-07 13:17:52 -0400

    Added encrypted api keys, arrival model, itinerary model, itineraries show route, itineraries controller, and migrations.

---
+++ db/migrate/20190507170748_create_itineraries.rb
@@ -0,0 +1,17 @@
+class CreateItineraries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :itineraries do |t|
+      t.string :source_code
+      t.string :source_name
+      t.string :destination_code
+      t.string :destination_name
+      t.float :miles
+      t.integer :time
+      t.float :peak_fare
+      t.float :off_peak_fare
+      t.float :senior_fare
+
+      t.timestamps
+    end
+  end
+end
