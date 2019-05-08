55cbfd18a1ac8163e5387005bbec69dabb338cd3
Dusty211 <kylebenz@hotmail.com>
2019-05-07 20:55:44 -0400

    Added station code to name converter module

--- app/controllers/api/v1/itineraries_controller.rb
+++ app/controllers/api/v1/itineraries_controller.rb
@@ -1,4 +1,4 @@
-require 'update_db.rb'
+require 'update_db'
 
 class Api::V1::ItinerariesController < ApplicationController
   def show
--- lib/update_db.rb
+++ lib/update_db.rb
@@ -1,5 +1,8 @@
+require 'station_converter'
 
 class UpdateDb
+
+  include StationConverter
 
   def initialize(source_code, destination_code)
     @source = source_code
@@ -14,6 +17,23 @@ class UpdateDb
     return_data(itinerary_url)
   end
 
+  def source_name
+    self.to_station_name(@source)
+  end
+
+  def destination_name
+    self.to_station_name(@destination)
+  end
+
+  # Dev use:
+  # def print_converter
+  #   output = {}
+  #     return_data(stations_url)["Stations"].each do |station|
+  #       output.merge!("#{station["Name"]}" => "#{station["Code"]}")
+  #     end
+  #   return output
+  # end
+
   private
 
   def api_key
@@ -27,6 +47,11 @@ class UpdateDb
   def itinerary_url
     return "https://api.wmata.com/Rail.svc/json/jSrcStationToDstStationInfo?api_key=#{api_key}&FromStationCode=#{@source}&ToStationCode=#{@destination}"
   end
+
+  # Dev use:
+  # def stations_url
+  #   return "https://api.wmata.com/Rail.svc/json/jStations?api_key=#{api_key}"
+  # end
 
   def return_data(url)
     response = RestClient.get(url)
