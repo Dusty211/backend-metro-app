482d1cc3eaba4af5a2c957d3d858d3d2a9eb771d
Dusty211 <kylebenz@hotmail.com>
2019-05-09 20:51:54 -0400

    basic itineraries routes and controller setup and tested.

--- app/controllers/api/v1/itineraries_controller.rb
+++ app/controllers/api/v1/itineraries_controller.rb
@@ -1,7 +1,15 @@
 require 'update_db'
 
 class Api::V1::ItinerariesController < ApplicationController
-  def show
-    # byebug
+  def find
+    render json: Itinerary.where(
+      source_code: itinerary_params[:source_code]).where(
+        destination_code: itinerary_params[:destination_code])
   end
+end
+
+private
+
+def itinerary_params
+  params.permit(:source_code, :destination_code)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :itineraries, only: [:show]
+      # resources :itineraries, only: [:show]
+      get 'itineraries/find', to: 'itineraries#find'
     end
   end
 end
