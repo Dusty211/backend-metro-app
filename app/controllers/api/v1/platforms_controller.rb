afdb461f0f1d37dbd485955652b7c3925065698b
Dusty211 <kylebenz@hotmail.com>
2019-05-19 16:24:36 -0400

    seperated source and destination platforms.

--- app/controllers/api/v1/platforms_controller.rb
+++ app/controllers/api/v1/platforms_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::PlatformsController < ApplicationController
 
-  def index
+  def source
     render json: SourcePlatform.all
+  end
+
+  def destination
+    render json: DestinationPlatform.all
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       # resources :itineraries, only: [:show]
-      resources :platforms, only: [:index]
+      # resources :platforms, only: [:source, :destination]
+      get 'platforms/source', to: 'platforms#source'
+      get 'platforms/destination', to: 'platforms#destination'
       get 'itineraries/find', to: 'itineraries#find'
     end
   end
