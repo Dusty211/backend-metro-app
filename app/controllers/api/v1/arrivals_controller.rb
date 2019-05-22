82c398cadf1e314745b6f4501a8ae6b8145999e0
Dusty211 <kylebenz@hotmail.com>
2019-05-22 16:43:49 -0400

    building arrivals #find controller method. Passing params and finding platform.

--- app/controllers/api/v1/arrivals_controller.rb
+++ app/controllers/api/v1/arrivals_controller.rb
@@ -3,13 +3,14 @@ require 'update_db'
 class Api::V1::ArrivalsController < ApplicationController
 
   def find
+    station = SourcePlatform.find_by(code: params["code"])
     # byebug
   end
 
   private
 
   def arrivals_params
-    params.permit(:platform_code, :alt_code)
+    params.permit(:code)
   end
 
 end
