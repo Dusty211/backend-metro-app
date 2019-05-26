8006cf90be08fe3716ef4490e8446d3d8b162174
Dusty211 <kylebenz@hotmail.com>
2019-05-26 14:34:19 -0400

    removed commented code in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,6 @@ puts "=> Seeded lines"
 UpdateDb.seed_platforms_addresses_associate_lines
 puts "=> Seeded platforms, addresses, and associated lines"
 
-# #depends on .seed_lines being done beforehand
-# UpdateDb.seed_platforms_addresses_associate_lines("destination")
-# puts "=> Seeded destination_platforms, addresses, and associated lines"
-
 #depends on .seed_platforms_addresses_associate_lines being done beforehand
 UpdateDb.seed_itineraries
 puts "=> Seeded itineraries"
