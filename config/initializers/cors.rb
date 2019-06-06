dc64d805c9e0a0c45818a4b94389ea91be8a75b1
EC2-28d955-simpleafmetro-api <kylebenz@hotmail.com>
2019-06-06 19:49:18 +0000

    fixed cors issues and tested. cors now working with www and without.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://*simpleafmetro.com'
+    origins 'www.simpleafmetro.com','simpleafmetro.com'
 
     resource '*',
       headers: :any,
