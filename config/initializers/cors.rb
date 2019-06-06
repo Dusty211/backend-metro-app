9fa623fa90e3a33580634f6ce7878390c4b97d7a
Dusty211 <kylebenz@hotmail.com>
2019-06-06 14:24:23 -0400

    updated cors.rb again. changed urls.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'simpleafmetro.com:443','www.simpleafmetro.com:443'
+    origins 'https://simpleafmetro.com:443','https://www.simpleafmetro.com:443'
 
     resource '*',
       headers: :any,
