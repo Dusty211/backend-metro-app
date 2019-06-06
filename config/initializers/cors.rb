b331b173d745ba4e01c79d04746db8ea283cf08e
Dusty211 <kylebenz@hotmail.com>
2019-06-06 15:43:36 -0400

    updated cors.rb again. added wildcard.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'www.simpleafmetro.com'
+    origins 'https://*simpleafmetro.com'
 
     resource '*',
       headers: :any,
