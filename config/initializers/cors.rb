29af2644042279f7f00c955b3eb8e9640cd63f6c
EC2-28d955-simpleafmetro-api <kylebenz@hotmail.com>
2019-06-07 00:41:05 +0000

    added option to allow cors wildcard by comment for testing.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,6 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'www.simpleafmetro.com','simpleafmetro.com'
+    #origins '*'
 
     resource '*',
       headers: :any,
