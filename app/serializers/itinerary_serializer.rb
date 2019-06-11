9791d7e1d33ae416bf18db9367e3016b9bda6cae
Dusty211 <kylebenz@hotmail.com>
2019-06-11 13:58:35 -0400

    added peaktime boolean to itinerary serializer

--- app/serializers/itinerary_serializer.rb
+++ app/serializers/itinerary_serializer.rb
@@ -1,4 +1,21 @@
 class ItinerarySerializer < ActiveModel::Serializer
-  attributes :source_code, :destination_code, :miles, :time, :peak_fare, :off_peak_fare, :senior_fare
+  attributes :source_code, :destination_code, :miles, :time, :peak_fare, :off_peak_fare, :senior_fare, :peak
+
+  def peak
+    weekday = Time.current.in_time_zone("UTC").in_time_zone("Eastern Time (US & Canada)").on_weekday?
+    current_time = Time.current.in_time_zone("UTC").in_time_zone("Eastern Time (US & Canada)")
+    peaktime = (
+        (current_time >= Time.find_zone("Eastern Time (US & Canada)").parse("05:00")) &&
+        (current_time < Time.find_zone("Eastern Time (US & Canada)").parse("09:30"))
+      ) || (
+        (current_time >= Time.find_zone("Eastern Time (US & Canada)").parse("15:00")) &&
+        (current_time < Time.find_zone("Eastern Time (US & Canada)").parse("19:00"))
+      )
+    if weekday && peaktime
+      return true
+    else
+      return false
+    end
+  end
 
 end
