9fe51c2421d887cdb984745adcdf9327dca09f54
Dusty211 <kylebenz@hotmail.com>
2019-08-09 15:28:41 -0400

    added notes in updateDb.rb

--- lib/update_db.rb
+++ lib/update_db.rb
@@ -1,14 +1,17 @@
 
+#This file contains the class used for programmatically seeding or re-seeding the database with the WMATA API.
+#It also contains several methods used by the controller to retrieve updated arrivals data and alerts from WMATA's API.
+
 class UpdateDb
 
-  def initialize(source_code, destination_code)
+  def initialize(source_code, destination_code) #Not needed currently
     @source = source_code
     @destination = destination_code
   end
 
   def self.seed_lines
-    lines = %w(GR BL SV RD OR YL)
-    color = {
+    lines = %w(GR BL SV RD OR YL) ##All WMATA lines that exist
+    color = { ##Converts WMATA abbreviations to full word
       "GR" => "green",
       "BL" => "blue",
       "SV" => "silver",
@@ -16,46 +19,47 @@ class UpdateDb
       "OR" => "orange",
       "YL" => "yellow"
      }
-    lines.each do |line|
+    lines.each do |line| #Create each line in the database with both WMATA code and full word
       Line.create(code: line, color: color[line])
     end
   end
 
   def self.seed_platforms_addresses_associate_lines
-    platforms_clone = all_platforms.clone
+    platforms_clone = all_platforms.clone #Use private method to fetch and clone
     platforms_clone.each do |platform|
-        new_platform = Platform.create(
+        new_platform = Platform.create( #Create platform row and save to variable
           code: platform["Code"],
           name: platform["Name"],
           alt_code: platform["StationTogether1"],
           lat: platform["Lat"],
           lon: platform["Lon"]
         )
-      platform_address = Address.create(
-        street: platform["Address"]["Street"],
-        city: platform["Address"]["City"],
-        state: platform["Address"]["State"],
-        zip: platform["Address"]["Zip"]
-      )
-      new_platform.address = platform_address
+        platform_address = Address.create( #Create address row and save to variable
+          street: platform["Address"]["Street"],
+          city: platform["Address"]["City"],
+          state: platform["Address"]["State"],
+          zip: platform["Address"]["Zip"]
+        )
+      new_platform.address = platform_address #Associate address with platform
       lines = []
       [1,2,3,4].each do |i|
-        #shovel existing line colors into lines variable
+        #shovel line colors into lines variable only if they exist
         platform["LineCode#{i}"] && lines << platform["LineCode#{i}"]
       end
-      #if station has multiple platforms
-      if platform["StationTogether1"] != ""
+
+      if platform["StationTogether1"] != "" #if station has multiple platforms
         #find alt platform and store in variable
         alt_platform = platforms_clone.find do |cloned|
           cloned["Code"] == platform["StationTogether1"]
         end
         [1,2,3,4].each do |i|
-          #shovel existing line colors from second platform into lines variable
+          #shovel existing line colors from second platform into same lines variable used above
           alt_platform["LineCode#{i}"] && lines << alt_platform["LineCode#{i}"]
         end
       end
+
       lines.sort!
-      #associate correct lines with the created db platform
+      #associate correct lines with the created platform and write to db
       lines.each do |line|
         new_platform.lines << Line.find_by(code: line)
       end
